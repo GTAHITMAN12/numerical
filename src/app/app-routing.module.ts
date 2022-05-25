d3cec0120abbe25037bbc057c36859dd45fd0526
“gtahitman12” <naatthawut24684@gmail.com>
2022-05-25 18:39:21 +0700

    ng 	src/app/component/ 	src/app/integate-diff-routing/ 	src/app/interextar-routing/ 	src/app/linear-routing/ 	src/app/newton.js 	src/app/root-routing/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InterExtarComponent } from './component/inter-extar/inter-extar.component';
+import { LinearAppComponent } from './component/linear-app/linear-app.component';
+import { RootAppComponent } from './component/root-app/root-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'root',component:RootAppComponent },
+  {path:'linear',component:LinearAppComponent },
+  {path:'interpotation and extrapolation',component:InterExtarComponent},
+  /*{path:'linear regression',component:},*/
+  /**/
+  /*{path:'integation and diff',component:  },*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,69 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { GraphicalMethodComponent } from './component/graphical-method/graphical-method.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { BisectionMethodComponent } from './component/bisection-method/bisection-method.component';
+import { OneFalsePositionComponent } from './component/one-false-position/one-false-position.component';
+import { OnePointIterationComponent } from './component/one-point-iteration/one-point-iteration.component';
+import { CramerRuleComponent } from './component/cramer-rule/cramer-rule.component';
+import { RootAppComponent } from './component/root-app/root-app.component';
+import { LinearAppComponent } from './component/linear-app/linear-app.component';
+import { RootRoutingModule } from './root-routing/root-routing.module';
+import { LinearRoutingModule } from './linear-routing/linear-routing.module';
+import { GaussComponent } from './component/gauss/gauss.component';
+import { GaussjordanComponent } from './component/gaussjordan/gaussjordan.component';
+import { SecantComponent } from './component/secant/secant.component';
+import { LudecomposeComponent } from './component/ludecompose/ludecompose.component';
+import { CholesskyComponent } from './component/cholessky/cholessky.component';
+import { JacobiComponent } from './component/jacobi/jacobi.component';
+import { GuassseidalComponent } from './component/guassseidal/guassseidal.component';
+import { ConjugateComponent } from './component/conjugate/conjugate.component';
+import { NewtonRaphsonComponent } from './component/newton-raphson/newton-raphson.component';
+import { InterExtarComponent } from './component/inter-extar/inter-extar.component';
+import { LinComponent } from './component/lin/lin.component';
+import { InterextarRoutingModule } from './interextar-routing/interextar-routing/interextar-routing.module';
+import { QinComponent } from './component/qin/qin.component';
+import { PinComponent } from './component/pin/pin.component';
+import { LilComponent } from './component/lil/lil.component';
+import { QilComponent } from './component/qil/qil.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphicalMethodComponent,
+    BisectionMethodComponent,
+    OneFalsePositionComponent,
+    OnePointIterationComponent,
+    CramerRuleComponent,
+    RootAppComponent,
+    LinearAppComponent,
+    GaussComponent,
+    GaussjordanComponent,
+    SecantComponent,
+    LudecomposeComponent,
+    CholesskyComponent,
+    JacobiComponent,
+    GuassseidalComponent,
+    ConjugateComponent,
+    NewtonRaphsonComponent,
+    InterExtarComponent,
+    LinComponent,
+    QinComponent,
+    PinComponent,
+    LilComponent,
+    QilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RootRoutingModule,
+    LinearRoutingModule,
+    ReactiveFormsModule,
+    InterextarRoutingModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
